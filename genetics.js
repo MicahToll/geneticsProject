b3f37044c8a17701775f3a629777b103f02cc37a
MicahToll <i.egxist@gmail.com>
2021-02-12 20:34:12 -0800

    added creature and human classes

--- genetics.js
+++ genetics.js
@@ -1,3 +1,44 @@
+//Class structure
+class creature{
+    animal_name = "";
+    gender;//can equal 'm' or 'f' (maybe 'a' or 'i' if needed as well)
+    traits = [];//this will be a list of traits 
+    constructor(animal_name, gender){
+        this.animal_name = animal_name;
+        this.gender = gender;
+    }
+    /*reproduce(){
+        return new 
+    }*/
+}
+
+class Human extends creature{
+    constructor(gender){
+        super('human', gender);
+    }
+}
+
+class dog extends creature{
+    constructor(gender){
+        super('dog', gender);
+    }
+}
+
+
+class trait{
+    trait_name = "";
+    constuctor(trait_name){
+        this.trait_name = trait_name;
+    }
+}
+/*
+example 1
+a person with blood type O
+they are a creature Human with the triats blood type
+*/
+bob = new Human('m');
+console.log(bob);
+
 function input(){
 	output("it worked");
 	outputLine("again");
